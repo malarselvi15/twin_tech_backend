52edf2e50b68243b6fa7a09a71188f981c6bdaac
malar <malarselvi273@gmail.com>
2026-01-22 14:28:55 +0530

    Fix Error

---
+++ src/main/java/com/example/Twintech/HealthController.java
@@ -0,0 +1,18 @@
+package com.example.Twintech;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
